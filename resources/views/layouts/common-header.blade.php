f42057cdcf727ba34f7d1569e656d406b7645b8e
波多野 雅哉 <masaya19880517@gmail.com>
2018-11-01 17:22:19 +0900

    テスト画面作成

--- resources/views/layouts/common-header.blade.php
+++ resources/views/layouts/common-header.blade.php
@@ -3,6 +3,9 @@
         <ul class="nav navbar-nav">
             <li><a href="/myList" class="header-link">Location MyList</a></li>
             <li><a href="/vue/todo/" class="header-link">TODO</a></li>
+            <li><a href="/" class="header-link">ADMIN</a></li>
+            <li><a href="/test" class="header-link">TEST</a></li>
+            <li><a href="/vue/test/" class="header-link">TEST VUE</a></li>
         </ul>
     </div>
 </nav>
--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<html lang="ja">
+
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<title>@yield('title')</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<meta name="csrf-token" context="{{ csrf_token() }}">
+<script>
+    window.Laravel = {};
+    window.Laravel.csrfToken = "{{ csrf_token() }}";
+</script>
 </head>
+</head>
+
 <body>
-    <div id="app">
-        <navbar></navbar>
-        <div class="container">
-            <router-view></router-view>
-        </div>
+    @include('layouts/common-header')
+    <div class="container">
+    <h1>test</h1>
+    @foreach ($data as $row)
+        @foreach ($row as $col)
+            <div>{{ $col }}</div>
+        @endforeach
+        <br/>
+    @endforeach
     </div>
 </body>
-<script src="{{ mix('js/app.js') }}"></script>
-</html>

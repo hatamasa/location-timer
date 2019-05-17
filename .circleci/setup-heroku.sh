bb40bb165cce599ebafac04964b9c1c61150f307
波多野 雅哉 <masaya19880517@gmail.com>
2019-05-17 12:44:18 +0900

    circleciデプロイ設定：herokuのリポジトリ設定を修正

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/<your-app-name>.git
+git remote add heroku https://git.heroku.com/location-timer.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
